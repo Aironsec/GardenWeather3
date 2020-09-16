45a4b9005f7ed3f845a12881acbfa5b7029f2b9b
Aironsec <61885959+Aironsec@users.noreply.github.com>
2020-09-17 02:52:22 +0300

    Part2lesson7_DZ

--- app/src/main/java/com/example/gardenweather3/OpenNameCityWeather.java
+++ app/src/main/java/com/example/gardenweather3/OpenNameCityWeather.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface OpenCurrentWeather {
+public interface OpenNameCityWeather {
     @GET("data/2.5/weather")
     Call<CurrentWeatherData> loadWeather(@Query("q") String city,
                                          @Query("units") String metric,
